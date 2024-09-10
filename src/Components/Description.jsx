af999559836b9f7fb3067c24f595553b177e1923
DawoudTormos <103008170+DawoudTormos@users.noreply.github.com>
2024-09-10 03:29:59 +0300

    Separating the example into Title, Description and Grid react Components.

---
+++ src/Components/Description.jsx
@@ -0,0 +1,12 @@
+import React from 'react'
+
+const Description = () => {
+  return (
+    <>
+          <p class="mb-12 text-lg text-gray-500">Here is a few of the awesome Services we provide.</p>
+
+    </>
+  )
+}
+
+export default Description
---
+++ src/Components/Title.jsx
@@ -0,0 +1,12 @@
+import React from 'react'
+
+const Title = () => {
+  return (
+    <>
+          <h2 class="mb-1 text-3xl font-extrabold leading-tight text-gray-900">Services</h2>
+
+    </>
+  )
+}
+
+export default Title
